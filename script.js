a8601d0af5fd077d4d9c4a82dc913ac1cefc5650
Simone-Gallucci <149935274+Simone-Gallucci@users.noreply.github.com>
2025-03-13 11:06:57 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    let message = document.getElementById("message");
+    message.innerText = "You have entered the shadows... Welcome to THE GHOST.";
+    message.style.opacity = 1;
+}
